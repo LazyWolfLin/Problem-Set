809b39697eb64aba4dcfcf7372e6087b0899b1e6
LazyWolf Lin <LazyWolfLin@gmail.com>
2017-01-14 22:10:32 +0800

    Solve Add Two Numbers by C++.

---
+++ LeetCode/C++/2. Add Two Numbers.cpp
@@ -0,0 +1,56 @@
+///LeetCode 2. Add Two Numbers C++
+///write by LazyWolfLin in 16.09.09
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//Definition for singly-linked list.
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x):val(x),next(NULL){}
+};
+
+class Solution
+{
+public:
+    ListNode* addTwoNumbers(ListNode *l1,ListNode *l2)
+    {
+        ListNode *ans=NULL,*tail;
+        //initialize
+        ans=new ListNode(l1->val+l2->val);
+        tail=ans;
+        int k=tail->val/10;
+        tail->val=tail->val%10;
+        //calculation
+        ListNode *pos_l1=l1->next;
+        ListNode *pos_l2=l2->next;
+        int val_l1,val_l2;
+        while (pos_l1!=NULL||pos_l2!=NULL)
+        {
+            if (pos_l1!=NULL)
+            {
+                val_l1=pos_l1->val;
+                pos_l1=pos_l1->next;
+            }
+            else val_l1=0;
+            if (pos_l2!=NULL)
+            {
+                val_l2=pos_l2->val;
+                pos_l2=pos_l2->next;
+            }
+            else val_l2=0;
+            tail->next=new ListNode(val_l1+val_l2+k);
+            tail=tail->next;
+            k=tail->val/10;
+            tail->val=tail->val%10;
+        }
+        if (k!=0)
+        {
+            tail->next=new ListNode(k);
+        }
+        return ans;
+    }
+};
