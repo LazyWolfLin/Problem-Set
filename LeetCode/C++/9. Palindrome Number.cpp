ad6cf22fd6b2a44e4293dcddccd8ac27f71e5529
LazyWolf Lin <LazyWolfLin@gmail.com>
2017-03-09 21:20:58 +0800

    Solve Palindrome Number by C++.

---
+++ LeetCode/C++/9. Palindrome Number.cpp
@@ -0,0 +1,19 @@
+///LeetCode 9. Palindrome Number by C++
+///write by LazyWolfLin in 17.03.09
+
+class Solution {
+public:
+    bool isPalindrome(int x)
+    {
+        if (x<0) return false;
+        int bit=1;
+        while (x/bit>=10) bit*=10;
+        while (x!=0)
+        {
+            if (x/bit!=x%10) return false;
+            x=(x%bit)/10;
+            bit=bit/100;
+        }
+        return true;
+    }
+};
