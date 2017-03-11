a1c101df5e7d24fa3d27084c42789d7a7f737dd2
LazyWolf Lin <LazyWolfLin@gmail.com>
2017-03-11 23:48:39 +0800

    Solve Container With Most Water by C++.

---
+++ LeetCode/C++/11. Container With Most Water.cpp
@@ -0,0 +1,17 @@
+///LeetCode 11. Container With Most Water by C++
+///write by LazyWolfLin in 17.03.11
+
+class Solution {
+public:
+    int maxArea(vector<int>& height)
+    {
+        int l=0,r=height.size()-1,ans=0;
+        while (l<r)
+        {
+            ans=max(ans,(r-l)*min(height[l],height[r]));
+            if (height[l]<height[r]) ++l;
+            else --r;
+        }
+        return ans;
+    }
+};
