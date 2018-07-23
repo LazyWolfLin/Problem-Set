85524c543e615cd1f3cba5b0e9a15b8b7a5ddbec
LazyWolf Lin <LazyWolfLin@gmail.com>
2018-07-23 22:28:41 +0800

    Update solution.

--- LeetCode/C++/3. Longest Substring Without Repeating Characters.cpp
+++ LeetCode/C++/3. Longest Substring Without Repeating Characters.cpp
@@ -1,5 +1,5 @@
-///LeetCode 3. Longest Substring Without Repeating Characters by C++
-///write by LazyWolfLin in 16.09.10
+/// LeetCode 3. Longest Substring Without Repeating Characters by C++
+/// write by LazyWolfLin in 16.09.10
 
 /*
 class Solution
@@ -8,23 +8,23 @@ public:
     bool letter[128];
     int lengthOfLongestSubstring(string s)
     {
-        size_t l=0,r=0;
-        int ans=0;
-        memset(letter,false,sizeof(letter));
-        while (l<s.size())
+        size_t l = 0, r = 0;
+        int ans = 0;
+        memset(letter, false, sizeof(letter));
+        while (l < s.size())
         {
-            while (r<s.size())
+            while (r < s.size())
             {
                 if (letter[s[r]]) break;
-                letter[s[r]]=true;
-                if (r-l+1>ans) ans=r-l+1;
+                letter[s[r]] = true;
+                if (r - l + 1 > ans) ans = r - l + 1;
                 ++r;
             }
-            while (l!=r)
+            while (l != r)
             {
-                letter[s[l]]=false;
+                letter[s[l]] = false;
                 ++l;
-                if (s[l-1]==s[r]) break;
+                if (s[l - 1] == s[ r ]) break;
             }
         }
         return ans;
@@ -39,16 +39,15 @@ public:
     int lengthOfLongestSubstring(string s)
     {
         //initialize
-        int l=0,r=0;
-        int ans=0;
-        memset(letter,0,sizeof(letter));
+        int l = 0, r = 0;
+        int ans = 0;
+        memset(letter, 0, sizeof(letter));
         //calculation
-        while (r<s.size())
+        while (r < s.size())
         {
-            l=max(l,letter[s[r]]);
-            ans=max(ans,r-l+1);
-            letter[s[r]]=r+1;
-            ++r;
+            l = max(l, letter[s[r]]);
+            ans = max(ans, r - l + 1);
+            letter[s[r]] = ++r;
         }
         return ans;
     }
